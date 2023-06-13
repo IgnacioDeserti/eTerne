2ff67bb88d9604f08103bafa1b9e4084bd16e0b3
ignacio tosini <ignaciotosini2002@gmail.com>
2023-06-13 16:09:49 -0300

    Cambio cookies

--- resources/js/components/Loader/Loader.jsx
+++ resources/js/components/Loader/Loader.jsx
@@ -1,10 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const Loader = () => {
+  const [showVideo, setShowVideo] = useState(false);
+  const [showImage, setShowImage] = useState(true);
+
   useEffect(() => {
     const loader = document.getElementById("loader-wrapper");
     const content = document.getElementById("content-wrapper");
+
+    if (document.cookie.indexOf("visited") >= 0) {
+      // El usuario ha visitado antes, muestra la imagen
+      setShowVideo(false);
+      setTimeout(() => {
+        setShowImage(false);
+        loader.style.display = "none";
+        content.classList.remove("hidden");
+      }, 1500);
+    } else {
+      // Esta es la primera visita del usuario, establece una cookie para rastrear visitas futuras
+      document.cookie = "visited=true";
+      setShowVideo(true);
+    }
 
     if (loader && content) {
       // Hacer visible la pantalla de carga durante 1.5 segundos
@@ -17,8 +34,11 @@ const Loader = () => {
 
   return (
     <div id="loader-wrapper">
-      {/* <img src="/storage/assets/logoEterne.png" alt="Loading..." /> */}
-      <video src="/storage/assets/videoLogoEterne.mp4" alt="Loading..." autoPlay muted/>
+      {showVideo ? (
+        <video src="/storage/assets/videoLogoEterne.mp4" alt="Loading..." autoPlay muted/>
+      ) : showImage ? (
+        <img src="/storage/assets/logoEterne.png" alt="Loading..." />
+      ) : null}
     </div>
   );
 }
